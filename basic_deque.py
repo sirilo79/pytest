1489f26c769e9c93df979aafe20bfeb4098acb26
sirilo79 <sirilo79@gmail.com>
2024-09-08 13:01:49 +0900

    basic

---
+++ basic_deque.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+deq = deque()
+
+deq.append(1)
+print(deq)
+
+deq.append(2)
+print(deq)
+
+deq.appendleft(3)
+print(deq)
+
+deq.appendleft(4)
+print(deq)
+
+deq.pop()
+print(deq)
+
+deq.popleft()
+print(deq)
+
+
+
+
+
+
